a6ff478db05633901b6a3d39448ada84df9ec03d
emredzx <36761435+emredzx@users.noreply.github.com>
2018-03-02 13:27:17 +0300

    Update HW1.py

--- Lecture1/HW1.py
+++ Lecture1/HW1.py
@@ -1,33 +1,35 @@
 e = int(input("Aralık giriniz. : "))
-for x in range(0, e):
-    print("Kullanıcının Girmiş Olduğu 2 Sayı Üzerinde 4 İşlem Yapabilen Uygulama")
-    sayi1 = int(input("1. Sayıyı Giriniz : "))
-    sayi2 = int(input("2. Sayıyı Giriniz : "))
-    islem = input("İşlem Seçiniz : {Toplama (+) - Çıkarma (-) - Çarpma (*) - Bölme(/) : ")
+if (e == 0):
+    print("Aralık 0 girilemez..")
+else:
+    for x in range(0, e):
+        print("Kullanıcının Girmiş Olduğu 2 Sayı Üzerinde 4 İşlem Yapabilen Uygulama")
+        sayi1 = int(input("1. Sayıyı Giriniz : "))
+        sayi2 = int(input("2. Sayıyı Giriniz : "))
+        islem = input("İşlem Seçiniz : {Toplama (+) - Çıkarma (-) - Çarpma (*) - Bölme(/) : ")
 
+        if (islem == "+"):
+            toplam = sayi1 + sayi2
+            print("Toplam : ", toplam)
 
-    if (islem == "+"):
-     toplam = sayi1 + sayi2
-     print("Toplam : ", toplam)
+        elif (islem == "-"):
+            fark = sayi1 - sayi2
+            print("Kalan : ", fark)
 
-    elif (islem == "-"):
-        fark = sayi1 - sayi2
-        print("Kalan : ", fark)
+        elif (islem == "*"):
 
-    elif (islem == "*"):
+            carp = sayi1 * sayi2
+            print("Kalan : ", carp)
 
-         carp = sayi1 * sayi2
-         print("Kalan : ", carp)
+        elif (islem == "/"):
+            if (sayi2 == 0):
+                print("Bölmede 2.sayı 0 girilemez..")
+            else:
+                bol = sayi1 // sayi2
+                print("Bölüm : ", bol)
 
-    elif (islem == "/"):
-        if (sayi2 == 0):
-            print ("Bölmede 2.sayı 0 girilemez..")
+        elif (islem == "exit"):
+            break
+
         else:
-            bol = sayi1 // sayi2
-            print("Bölüm : ", bol)
-
-    elif (islem == "exit"):
-        break
-
-    else:
-         print("Bir işlem seçiniz...")
+            print("Bir işlem seçiniz...")
