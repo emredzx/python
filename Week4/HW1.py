8e0d1318284e48130438b25e75c1b7d0fea61b47
emredzx <36761435+emredzx@users.noreply.github.com>
2018-03-29 21:12:07 +0300

    Week4

--- Week4/HW1.py
+++ Week4/HW1.py
@@ -1,4 +1,4 @@
-class Calculator:
+class Stack:
     def __init__(self):
         self.items = []
 
@@ -6,7 +6,7 @@ class Calculator:
         return self.items == []
 
     def push(self, item):
-        self.items.insert(0,item)
+        self.items.insert(0, item)
 
     def pop(self):
         return self.items.pop(0)
@@ -14,32 +14,39 @@ class Calculator:
     def peek(self):
         return self.items[0]
 
-    def infix2Postfix(self,infix):
+    def size(self):
+        return len(self.items)
+
+
+class Calculator:
+    def infix2Postfix(self, infix):
         prec = {}
         prec["*"] = 2
         prec["/"] = 2
         prec["+"] = 1
         prec["-"] = 1
-        self.Stack = Calculator()
+        self.Stack = Stack()
         self.PostFix = []
-        self.TempArr=[]
-        self.infix=infix
-        infix=infix.replace(" ","")
-        inlen=len(infix)
-        for arr in range (inlen):
+        self.TempArr = []
+        self.infix = infix
+        infix = infix.replace(" ", "")
+        inlen = len(infix)
+        for arr in range(inlen):
             self.TempArr.append(infix[arr])
         for value in self.TempArr:
             if value in "0123456789":
                 self.PostFix.append(value)
             else:
                 while (not self.Stack.isEmpty()) and \
-                 (prec[self.Stack.peek()] >= prec[value]):
+                        (prec[self.Stack.peek()] >= prec[value]):
                     self.PostFix.append(self.Stack.pop())
                 self.Stack.push(value)
         while not self.Stack.isEmpty():
             self.PostFix.append(self.Stack.pop())
         return " ".join(self.PostFix)
 
-inf=input("Infix giriniz : ")
-pfix=Calculator().infix2Postfix(inf)
-print("Postfix : "+pfix)
+
+if __name__ == "__main__":
+    inf = input("Infix giriniz : ")
+    pfix = Calculator().infix2Postfix(inf)
+    print("Postfix : " + pfix)
